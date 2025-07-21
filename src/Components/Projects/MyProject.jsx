78c578445016ed7b0cbd2d8b4e124a96462264ea
Koushik Mehta <koushikmehta07@gmail.com>
2025-07-21 16:17:27 +0530

    Update MyProject.jsx

--- src/Components/Projects/MyProject.jsx
+++ src/Components/Projects/MyProject.jsx
@@ -7,16 +7,16 @@ const projects = [
   title: "PAN & Aadhaar Card Fraud Detection",
   description:
     "Created and structured employee datasets in MS Excel and performed data cleaning and transformation using MySQL. Managed and validated data to support fraud detection rules related to PAN and Aadhaar cards. Imported the cleaned data into Power BI and developed an interactive dashboard to visualize fraud detection patterns and insights.",
-  image: "Images/pan-aadhaar-fraud.png", // Replace with the actual image file
-  github: "https://github.com/KoushikMehta31/Pan-Aadhaar-Fraud-Detection", // Update if different
+  image: "Images/dashboard.png",
+  github: "https://github.com/KoushikMehta31/PAN-and-Aadhaar-Card-Fraud-Detection",
   skills: ["MS Excel", "SAP BIS", "Data Cleaning", "MySQL", "Power BI"]
 },
   {
   title: "Indian Bike Sales Analysis Using Excel",
   description:
     "Analyzed Indian bike sales data from Kaggle using Excel to derive insights on brand performance and sales trends. Cleaned the dataset by handling duplicates and inconsistencies to ensure accuracy. Created an interactive dashboard using Pivot Tables and Excel charts for dynamic and insightful data visualization.",
-  image: "Images/bike-sales-analysis.png", // Replace with your actual image path
-  github: "https://github.com/KoushikMehta31/Indian-Bike-Sales-Excel", // Update with actual GitHub repo
+  image: "Images/dashboardexcel.png",
+  github: "https://github.com/KoushikMehta31/Indian-Bike-Sales-Analysis-Using-Excel",
   skills: ["MS Excel", "Pivot Tables", "Excel Charts", "Data Visualization","Data Cleaning", "Kaggle"]
 },
   {
@@ -24,7 +24,7 @@ const projects = [
   description:
     "Performed Exploratory Data Analysis (EDA) on a placement dataset sourced from Kaggle using Python and Pandas. Cleaned and preprocessed data by handling null values, duplicates, and inconsistent entries. Created insightful visualizations using Matplotlib and Seaborn to uncover key trends and factors affecting student placements.",
   image: "Images/placement.png", // Replace with the actual image path
-  github: "https://github.com/KoushikMehta31/EDA-Placement-Analysis", // Update if different
+  github: "https://github.com/KoushikMehta31/EDA-Placement-Analysis-using-Python", // Update if different
   skills: ["Python", "Pandas", "Matplotlib", "Seaborn", "EDA", "Data Visualization","Data Cleaning","Kaggle"]
 },
   {
@@ -32,7 +32,7 @@ const projects = [
   description:
     "Performed analysis on student performance data to understand how factors like gender, parental education, and test preparation affect exam scores. Used Python and pandas for data cleaning and preprocessing. Conducted EDA and created visualizations to explore relationships between different variables.",
   image: "Images/student.png", // Replace with actual image path
-  github: "https://github.com/KoushikMehta31/Student-Performance-EDA", // Update if different
+  github: "https://github.com/KoushikMehta31/Students-Performance-in-Exams", // Update if different
   skills: ["Python", "Pandas", "Matplotlib", "Seaborn", "EDA", "Data Cleaning"]
 },
   {
@@ -40,7 +40,7 @@ const projects = [
   description:
     "Performed data analysis on e-commerce sales data to uncover insights on product performance, customer behavior, and regional trends. Cleaned and explored the dataset using Python and Pandas. Created interactive visualizations using Matplotlib and Seaborn to identify top-selling products, sales distribution, and category-wise performance.",
   image: "Images/ecommerce.png", // Replace with actual image path
-  github: "https://github.com/KoushikMehta31/Ecommerce-Sales-Analysis", // Update if different
+  github: "https://github.com/KoushikMehta31/Exploratory-Data-Analysis-On-E-Commerce-Data", // Update if different
   skills: ["Python", "Pandas", "Matplotlib", "Seaborn", "EDA", "Data Visualization","Data Cleaning"]
 },
   {
