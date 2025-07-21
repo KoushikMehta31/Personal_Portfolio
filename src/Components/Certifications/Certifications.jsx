85a59840a9798135110d5ada05b392994304bb72
Koushik Mehta <koushikmehta07@gmail.com>
2025-07-21 16:46:45 +0530

    Update Certifications.jsx

--- src/Components/Certifications/Certifications.jsx
+++ src/Components/Certifications/Certifications.jsx
@@ -9,43 +9,41 @@ const Certifications = () => {
       image: "Images/data.jpg", 
       title: "Data Science & Analytics",
       platform: "HP LIFE | HP Foundation",
-      status: "Completed",
     },
     {
       image: "Images/dsa.jpg", 
       title: "DSA With Java",
       platform: "Apna College",
-      status: "Completed",
     },
     {
       image: "Images/WebDevApnaCollege.jpg", 
       title: "DSA With Java",
       platform: "Apna College",
-      status: "Completed",
+      
     },
     {
       image: "Images/LeetCode.jpg", 
       title: "Top LeetCode Interview Questions",
       platform: "Udemy",
-      status: "Completed",
+      
     },
     {
       image: "Images/Git_Github.jpg",  
       title: "Git and Github - Beginners to Expert",
       platform: "Udemy",
-      status: "Completed",
+      
     },
     {
       image: "Images/java.jpeg", 
       title: "Introduction To Java",
       platform: "LearnTube",
-      status: "Completed",
+      
     },
     {
       image: "Images/oop.jpg",
       title: "Object Oriented Programming",
       platform: "Great Learning",
-      status: "Completed",
+      
     },
   ];
 
@@ -63,9 +61,7 @@ const Certifications = () => {
             <div className="certificate-details">
               <h3>{certificate.title}</h3>
               <p className="platform">{certificate.platform}</p>
-              <p className="date">
-                <span className="calendar-icon">📅</span> {certificate.date}
-              </p>
+      
             </div>
           </div>
         ))}
